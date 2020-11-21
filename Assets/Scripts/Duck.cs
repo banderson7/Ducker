548e915527f504ea7789ea2f38a5ffed6ba981b0
Bill Anderson <billanderson887@gmail.com>
2020-11-20 23:27:00 -0600

    Spawning and moving vehicles, kills duck (#3)
    Co-authored-by: banders <banderson7@users.noreply.github.com>

---
+++ Assets/Scripts/Duck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class Duck : MonoBehaviour
+{
+    private Input _input;
+    private Renderer _renderer;
+    private Collider _collider;
+
+    private void Awake()
+    {
+        _input = GetComponent<Input>();
+        _renderer = GetComponent<MeshRenderer>();
+        _collider = GetComponent<Collider>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.CompareTag("Vehicle"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        _input.StopDuck();
+        gameObject.SetActive(false);
+    }
+}

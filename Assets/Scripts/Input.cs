da8b4c31c2531c8383c3afdaca9199b5100f25d7
banders <banderson7@users.noreply.github.com>
2020-11-20 21:44:31 -0600

    Movement and turning

---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input : MonoBehaviour
+{
+    private Controls _controls;
+    private Movement _movement;
+
+    private void Awake()
+    {
+        _controls = new Controls();
+        _movement = GetComponent<Movement>();
+    }
+    private void Start()
+    {
+        _controls.Duck.Move.performed += ctx => _movement.MoveInput(ctx.ReadValue<Vector2>());
+        _controls.Duck.Move.canceled += ctx => _movement.MoveInput(ctx.ReadValue<Vector2>());
+        
+        _controls.Duck.Enable();
+    }
+}

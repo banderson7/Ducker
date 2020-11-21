24b9f6b37fde57abc49ab85b4fa12388b88422e0
Bill Anderson <billanderson887@gmail.com>
2020-11-20 22:00:16 -0600

    Camera following duck (#2)
    Co-authored-by: banders <banderson7@users.noreply.github.com>

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 _offset;
+
+    private void Awake()
+    {
+        _offset = transform.position - target.transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = target.position + _offset;
+    }
+}
